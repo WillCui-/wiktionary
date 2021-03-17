5f527375cc7d89d3949bfb136429ba2c6329e241
WillCui- <wyfcui@gmail.com>
2021-03-17 15:44:52 -0400

    language support

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Wiktionary Chrome extension
 
-Type \` in the address bar to search Wiktionary
+Chrome extension to quickly query Wiktionary
+
+Ways to search:
+* Type <kbd>`</kbd> + <kbd>Space</kbd> in the address bar
+* Use the extension search bar
+* Right-click a highlighted word and select the context menu option
+
+Supports all active Wiktionarys (150+ Languages)
--- background.js
+++ background.js
@@ -1,12 +1,26 @@
-let language = 'en';
+var language = 'en';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ "language": language });
-});
-
-chrome.omnibox.onInputEntered.addListener((searchText) => {
+function searchWiki(searchText) {
   chrome.storage.sync.get("language", ({ language }) => {
     let url = 'https://' + language + '.wiktionary.org/wiki/' + searchText;
     chrome.tabs.update({ url: url });
   });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ "language": language });
+
+  chrome.contextMenus.create({
+    id: "wiktionary",
+    title: "Search Wiktionary for \'%s\'",
+    contexts: ["selection"],
+  });
+});
+
+chrome.omnibox.onInputEntered.addListener((searchText) => {
+  searchWiki(searchText);
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  searchWiki(info.selectionText);
 });
